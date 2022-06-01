4b9a347e761548824c56c0bdb4d6034a2c56d17d
doctype-Melvin <doctype.melvin@gmail.com>
2022-06-01 13:59:25 +0200

    adds basic homepage

--- src/index.js
+++ src/index.js
@@ -25,6 +25,9 @@ const makeContent = (()=>{
         const btnMenu = elementFactory('button', 'Btn');
         const btnAbout = elementFactory('button', 'Btn');
     const main = elementFactory('div', 'main');
+        const modal = elementFactory('div', 'modal');
+        const modalH = elementFactory('h1', 'modalHeader');
+        const modalP = elementFactory('p', 'modalText')
     const footer = elementFactory('footer', 'footer');
 return{
     header,
@@ -34,16 +37,23 @@ return{
     btnMenu,
     main,
     footer,
-    btnCt
+    btnCt,
+    modal,
+    modalH,
+    modalP
 }
 })()
 
 
 //Element edits
+makeContent.header.textContent = 'Smokehouse BBQ';
 makeContent.nav.append(makeContent.btnCt)
 makeContent.btnCt.append(makeContent.btnHome, makeContent.btnMenu, makeContent.btnAbout) //Creates buttons in nav element
 makeContent.btnHome.textContent = 'HOME';
 makeContent.btnMenu.textContent = 'MENU';
-makeContent.btnAbout.textContent = 'ABOUT'
-makeContent.header.textContent = 'Smokehouse BBQ';
+makeContent.btnAbout.textContent = 'ABOUT';
+makeContent.main.append(makeContent.modal);
+makeContent.modal.append(makeContent.modalH, makeContent.modalP);
+makeContent.modalH.textContent = 'The highest quality BBQ Meat in all of Otter Tail MN';
+makeContent.modalP.textContent = 'Come visit us and enjoy our flavorful and juicy flame grilled Beef, Bison, Chicken and Fish.'
 makeContent.footer.textContent = '© 2022 doctype-Melvin'
